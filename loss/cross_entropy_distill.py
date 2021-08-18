f5ec4e65b11c92935a53b7bc8ac340d60f55f68d
Andres Velez <anvelezec@unal.edu.co>
2021-08-18 16:27:44 -0500

    cross entropy distill doc

--- loss/cross_entropy_distill.py
+++ loss/cross_entropy_distill.py
@@ -1,7 +1,18 @@
 import torch
 
 # Linear combination in the loss function
-def cross_entropy_distill(s_outputs, t_outputs, labels, alpha):
+def cross_entropy_distill(s_outputs: torch.Tensor, t_outputs: torch.Tensor, labels:torch.Tensor, alpha:float) -> torch.Tensor:
+    """
+    Blended loss function. Mix between MSE and cross entropy
+    Args:
+        s_outputs (torch.Tensor): Student output loggits
+        t_outputs (torch.Tensor): Teacher output loggits 
+        labels (torch.Tensor): Ground thruth labels
+        alpha (float): MSE and cross entropy mix coheficient factor 
+
+    Returns:
+        torch.Tensor: Mixed loss
+    """
     criterion_mse = torch.nn.MSELoss()
     criterion_ce = torch.nn.CrossEntropyLoss()
 
